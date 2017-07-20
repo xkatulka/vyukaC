a5a1bfb0d60721794d7888a0aa02e231a7ee4396
xkatulka <xkatulka@users.noreply.github.com>
2017-07-20 08:06:16 +0200

    pridani cviceni 1

---
+++ cv1/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_19da5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_19da5.dir/feature_tests.cxx.obj"
+  "cmTC_19da5.pdb"
+  "cmTC_19da5.exe"
+  "cmTC_19da5.exe.manifest"
+  "libcmTC_19da5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_19da5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
