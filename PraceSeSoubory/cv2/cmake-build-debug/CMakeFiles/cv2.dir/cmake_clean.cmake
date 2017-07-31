e751fe03bac4b1dec28df5e2300ea577a8876ca2
xkatulka <xkatulka@users.noreply.github.com>
2017-07-31 14:16:39 +0200

    cv1

---
+++ PraceSeSoubory/cv2/cmake-build-debug/CMakeFiles/cv2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv2.dir/main.c.obj"
+  "cv2.pdb"
+  "cv2.exe"
+  "cv2.exe.manifest"
+  "libcv2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cv2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- PraceSeSoubory/cv2/main.c
+++ PraceSeSoubory/cv2/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+/*
+ #include <stdio.h>
+
+int main() {
     // cv 1
 
     FILE *fr; // soubor pro cteni
@@ -11,21 +18,13 @@ int main() {
         return 1; // nastala chyba otevirani souboru, program konci
     }
     do{
-        switch(c = getc(fr)){
-            case '0':
-                printf("Byla to nula.\n");
-                break;
-            case '1':
-                printf("Byla to jednicka.\n");
-                break;
-            default:
-                break;
-        }
-    }while ((c != 'q') && (c != EOF));
+        c = getc(fr);
+        printf("%c", c);
+    }while (c != 'q');
 
     if(fclose(fr) == EOF){
         printf("Soubor znaky.txt se nepodarilo zavrit!");
         return 1; // nastala chyba zavirani souboru, program konci
     }
     return 0;
-}
+} */
